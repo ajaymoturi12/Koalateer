256cb7ae0e5da2a63677a79954b16a1aaf928ce8
ajaymoturi12 <ajaymoturi0@gmail.com>
2022-09-20 15:52:21 -0400

    initial project creation files

---
+++ Koalateer/KoalateerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoalateerApp.swift
+//  Koalateer
+//
+//  Created by Ajay Moturi on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct KoalateerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
