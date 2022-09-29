34a4ca7cf6833538965db84f261e78d4e0a7049e
ajaymoturi12 <ajaymoturi0@gmail.com>
2022-09-29 13:43:00 -0400

    created login view with basic fields

--- Koalateer/ContentView.swift
+++ Koalateer/ContentView.swift
@@ -12,23 +12,51 @@ struct ContentView: View {
     @State var email = ""
     @State var password = ""
     @State var signIn: Bool=false
-    
+        
     var body: some View {
         VStack {
-            TextField("Email:", text: self.$email)
+            Text("Koalateer")
+                .font(.system(size:40, weight:.medium, design:.rounded))
+
+            Image("Koala")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
                 .padding()
             
-            SecureField("Password: ", text: self.$password)
-                .padding()
+            TextField(" Email:", text: self.$email)
+                .frame(width: 300, height: 50)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .overlay(RoundedRectangle(cornerRadius: 12)
+                .stroke(Color(.systemGray3), lineWidth: 1)
+                .foregroundColor(.clear))
+                .padding([.top], 50)
             
-           
-            NavigationLink(destination: SwiftUIView(), isActive: self.$signIn) {
+            SecureField(" Password: ", text: self.$password)
+                .frame(width: 300, height: 50)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .overlay(RoundedRectangle(cornerRadius: 12)
+                .stroke(Color(.systemGray3), lineWidth: 1)
+                .foregroundColor(.clear))
+                .padding()
                 
+            
+            NavigationLink(value: signIn) {
                 Button(action: {
                     signIn = true
                 }, label: {
                     Text("Sign In")
+                        .frame(width: 100, height: 50)
+                        .foregroundColor(.white)
+                        .background(.green)
+                        .cornerRadius(15)
                 })
+                
+                .navigationDestination(isPresented: $signIn){
+                    SwiftUIView()
+                }
             }
         }
     }
